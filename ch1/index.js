0baa2fbab54e9a676a6fdc5f6c6023d7c7b16fe3
Atul Agnihotri <atulhts11@gmail.com>
2024-04-08 00:17:39 +0530

    adding ch1

---
+++ ch1/index.js
@@ -0,0 +1,9 @@
+console.log("Hello Atul");
+console.log("Hello Atul");
+console.log("Hello Atul");
+console.log("Hello Atul");
+console.log("Hello Atul");
+
+for(var i = 0; i<20; i++){
+    console.log(`Atul Agnihotri ${i}`);
+}
