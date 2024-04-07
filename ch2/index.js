6514dc07d0635dd4eb2645b7ac8a737309719299
Atul Agnihotri <atulhts11@gmail.com>
2024-04-08 00:20:58 +0530

    commiting ch2

---
+++ ch2/index.js
@@ -0,0 +1,16 @@
+// (function(exports, require, module, __filename, __dirname){
+//     //module code actually lives here
+// })
+
+
+// console.log(__dirname);
+// console.log(module);
+
+// console.log(__filename);
+
+const path = require('path');
+// console.log(path);
+
+
+
+console.log((path.basename('/ch2/index.js','.js')));
