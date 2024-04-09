2998e27efebfc41ed9bd6d32ebd84a040e3877fd
Atul Agnihotri <atulhts11@gmail.com>
2024-04-09 23:27:07 +0530

    learn include files in ejs

---
+++ Express/ch13_include/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+const app = express()
+const port = process.env.PORT || 3000
+import web from './routes/web.js'
+
+
+// Set template Engine
+app.set('view engine', 'ejs')
+
+// Load Routes
+app.use('/',web)
+
+app.listen(port, ()=>{
+    console.log(`Listen on http://localhost:${port}`);
+})
